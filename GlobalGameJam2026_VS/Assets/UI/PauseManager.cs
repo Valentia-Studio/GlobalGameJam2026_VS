7d8d7e6d85092825067b527bab2449690eb10da7
vicenteperis1 <vicenteperis6@gmail.com>
2026-01-31 17:19:33 +0100

    UI
    Menu inicio y menu pausa maquetado completo

--- GlobalGameJam2026_VS/Assets/UI/PauseManager.cs
+++ GlobalGameJam2026_VS/Assets/UI/PauseManager.cs
@@ -4,7 +4,6 @@ public class PauseManager : MonoBehaviour
 {
     public static PauseManager Instance { get; private set; }
     public GameObject pauseMenuUI; // Asigna tu Canvas de pausa aquí
-    private bool isPaused = false;
 
     void Awake()
     {
@@ -20,26 +19,5 @@ public class PauseManager : MonoBehaviour
         }
     }
 
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            if (isPaused) Resume();
-            else Pause();
-        }
-    }
 
-    public void Resume()
-    {
-        pauseMenuUI.SetActive(false);
-        Time.timeScale = 1f; // Reanuda el tiempo
-        isPaused = false;
-    }
-
-    void Pause()
-    {
-        pauseMenuUI.SetActive(true);
-        Time.timeScale = 0f; // Pausa el tiempo
-        isPaused = true;
-    }
 }
--- GlobalGameJam2026_VS/Assets/UI/PauseMenu.cs
+++ GlobalGameJam2026_VS/Assets/UI/PauseMenu.cs
@@ -4,16 +4,24 @@ public class PauseMenu : MonoBehaviour
 {
     [SerializeField] private GameObject pauseMenuUI;
     [SerializeField] private GameObject quite;
+    [SerializeField] private GameObject options;
+    private bool isPaused = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused) Resume();
+            else Pause();
+        }
     }
+
 
     public void Empezar()
     {
@@ -24,8 +32,9 @@ public class PauseMenu : MonoBehaviour
     public void Continuar()
     {
         Time.timeScale = 1f; // Resume the game
-        pauseMenuUI.SetActive(false); 
+        pauseMenuUI.SetActive(false);
         NoSalir();
+        NonOptions();
 
     }
 
@@ -49,4 +58,35 @@ public class PauseMenu : MonoBehaviour
         // carga la escen  específica
         UnityEngine.SceneManagement.SceneManager.LoadScene(scene);
     }
+
+
+
+
+    public void Options()
+    {
+        options.SetActive(true);
+    }
+
+    public void NonOptions()
+    {
+        options.SetActive(false);
+    }
+
+
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f; // Reanuda el tiempo
+        isPaused = false;
+        Continuar();
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f; // Pausa el tiempo
+        isPaused = true;
+    }
+
 }
